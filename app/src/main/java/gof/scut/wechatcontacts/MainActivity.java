4b2ea3201294e9f27d20e3270e04c6541938d434
Lozyboobob <137150808@qq.com>
2015-04-23 21:23:55 +0800

    4.23

--- app/src/main/java/gof/scut/wechatcontacts/MainActivity.java
+++ app/src/main/java/gof/scut/wechatcontacts/MainActivity.java
@@ -15,102 +15,102 @@ import gof.scut.cwh.models.adapter.ContactsAdapter;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-	//Views
-	private Button btSearch, btMenu;
-
-	private ListView contacts;
-	private Button btAdd, btGroup, btMe;
-
-
-	//Constants
-
-	//Models
-	MainTableUtils mainTableUtils;
-
-	//Controllers
+    //Views
+    private Button btSearch, btMenu;
+    private MyLetterListView letterListView;
+    private ListView contacts;
+    private Button btAdd, btGroup, btMe;
 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		init();
-	}
+    //Constants
 
-	private void init() {
-		//init database
-		initDatabase();
-		//init Views
-		findViews();
-		//init handlers //init button listeners, list
-		eventHandler();
-	}
+    //Models
+    MainTableUtils mainTableUtils;
 
-	private void initDatabase() {
-		mainTableUtils = new MainTableUtils(this);
+    //Controllers
 
-	}
 
-	private void findViews() {
-		btSearch = (Button) findViewById(R.id.bt_search);
-		btMenu = (Button) findViewById(R.id.bt_menu);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
 
-		contacts = (ListView) findViewById(R.id.search_list);
-		btAdd = (Button) findViewById(R.id.bt_add);
-		btGroup = (Button) findViewById(R.id.bt_group);
-		btMe = (Button) findViewById(R.id.bt_me);
-	}
+    private void init() {
+        //init database
+        initDatabase();
+        //init Views
+        findViews();
+        //init handlers //init button listeners, list
+        eventHandler();
+    }
 
-	private void eventHandler() {
-		btSearch.setOnClickListener(this);
-		btMenu.setOnClickListener(this);
+    private void initDatabase() {
+        mainTableUtils = new MainTableUtils(this);
 
-		btSearch.setOnClickListener(this);
-		btAdd.setOnClickListener(this);
-		btGroup.setOnClickListener(this);
-		btMe.setOnClickListener(this);
+    }
 
-	}
+    private void findViews() {
+        btSearch = (Button) findViewById(R.id.bt_search);
+        btMenu = (Button) findViewById(R.id.bt_menu);
+        letterListView = (MyLetterListView) findViewById(R.id.my_list_view);
+        contacts = (ListView) findViewById(R.id.search_list);
+        btAdd = (Button) findViewById(R.id.bt_add);
+        btGroup = (Button) findViewById(R.id.bt_group);
+        btMe = (Button) findViewById(R.id.bt_me);
+    }
 
-	private void initList() {
-		//When clickOnNameAndTel, view
-		//When click on image, view label group
-		//edit when view
-		Cursor cursor = mainTableUtils.selectAllIDName();
-		ContactsAdapter adapter = new ContactsAdapter(this, cursor);
-		contacts.setAdapter(adapter);
-	}
+    private void eventHandler() {
+        btSearch.setOnClickListener(this);
+        btMenu.setOnClickListener(this);
 
-	@Override
-	public void onClick(View v) {
+        btSearch.setOnClickListener(this);
+        btAdd.setOnClickListener(this);
+        btGroup.setOnClickListener(this);
+        btMe.setOnClickListener(this);
 
-		switch (v.getId()) {
-			case R.id.bt_search:
-				ActivityUtils.ActivitySkip(this, SearchActivity.class);
+    }
 
-				break;
-			case R.id.bt_menu:
-				//A MENU WITH SETTING IN IT
-				ActivityUtils.ActivitySkip(this, SettingActivity.class);
-				break;
-			case R.id.bt_add:
-				//TO ADD CONTACTS ACTIVITY
-				ActivityUtils.ActivitySkip(this, AddContactActivity.class);
-				break;
-			case R.id.bt_group:
-				//TO GROUP VIEW
-				ActivityUtils.ActivitySkip(this, LabelsActivity.class);
-				break;
-			case R.id.bt_me:
-				//TO SELF ACTIVITY
-				ActivityUtils.ActivitySkip(this, SelfInfoActivity.class);
-				break;
-		}
-	}
+    private void initList() {
+        //When clickOnNameAndTel, view
+        //When click on image, view label group
+        //edit when view
+        Cursor cursor = mainTableUtils.selectAllIDName();
+        ContactsAdapter adapter = new ContactsAdapter(this, cursor);
+        contacts.setAdapter(adapter);
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		initList();
-	}
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.bt_search:
+                ActivityUtils.ActivitySkip(this, SearchActivity.class);
+
+                break;
+            case R.id.bt_menu:
+                //A MENU WITH SETTING IN IT
+                ActivityUtils.ActivitySkip(this, SettingActivity.class);
+                break;
+            case R.id.bt_add:
+                //TO ADD CONTACTS ACTIVITY
+                ActivityUtils.ActivitySkip(this, AddContactActivity.class);
+                break;
+            case R.id.bt_group:
+                //TO GROUP VIEW
+                ActivityUtils.ActivitySkip(this, LabelsActivity.class);
+                break;
+            case R.id.bt_me:
+                //TO SELF ACTIVITY
+                ActivityUtils.ActivitySkip(this, SelfInfoActivity.class);
+                break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initList();
+    }
 }
